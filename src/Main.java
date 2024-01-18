0ba112691cad4e85cbca080b77f858ecf6ce8784
Ullrich1 <andreullrich715@gmail.com>
2024-01-18 13:45:52 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int vet[] = new int[11];
+        int cont = 10;
+
+        System.out.println("Os elementos impares no intervalo são: ");
+
+        for (int i=0; i<11; i++) {
+            vet[i] = cont;
+            cont++;
+        }
+
+        for (int i=0; i<11; i++) {
+            if (vet[i]%2!=0){
+                System.out.print(vet[i] + "\t");
+            }
+        }
+    }
+}
